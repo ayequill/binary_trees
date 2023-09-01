19f6de0711a802bb4e87eabe5dd4a49f0c40e379
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-01 11:01:24 +0000

    Removed mixed declaration

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,8 +12,11 @@ size_t calculate_height(const binary_tree_t *node)
 {
 	if (!node)
 		return (0);
-	size_t left_height = calculate_height(node->left);
-	size_t right_height = calculate_height(node->right);
+	size_t left_height;
+	size_t right_height;
+
+	left_height = calculate_height(node->right);
+	right_height = calculate_height(node->left);
 
 	return ((left_height > right_height) ?
 		(left_height + 1) :
